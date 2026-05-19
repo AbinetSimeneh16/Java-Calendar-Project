c13702c497ede69082f942912758bbb10f92090f
AbinetSimeneh16 <abinetsimeneh1621@gmail.com>
2026-05-19 08:23:50 -0700

    implemented calendar display for month and year input of months that start their day from sunday

--- src/calender.java
+++ src/calender.java
@@ -1,3 +1,5 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Scanner;
 public class calender {
     public static void main(String[] args)
@@ -32,6 +34,77 @@ public class calender {
             else {
                 break;
             }
+        }
+        int days;
+        if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12 )
+        {
+            days=31;
+        }
+        else if (month==4 || month==6 || month==9 || month==11)
+        {
+            days=30;
+        }
+        else
+        {
+            if((year%4==0 && year%100 !=0) || year% 400==0)
+            {
+                days=29;
+            }
+            else
+            {
+                days=28;
+            }
+        }
+        LocalDate date=LocalDate.of(year,month,1);
+        DayOfWeek day=date.getDayOfWeek();
+        String text=String.valueOf(year);
+        String t=String.valueOf(month);
+        System.out.println(text + " "+ t);
+        System.out.println('S'+"   "+'M'+"   "+'T'+"   "+'W'+"   "+'T'+"   "+'F'+"   "+'S'+"   ");
+        int z;
+        if( days==31)
+        {
+            z=31;
+        }
+        else if (days==30)
+        {
+            z=30;
+        }
+        else
+        {
+            z=29;
+        }
+        int[] d=new int[z];
+        for(int i=0;i<z;i++)
+        {
+            d[i]=i+1;
+        }
+        int space=0;
+
+        if(day==DayOfWeek.SUNDAY)
+        {
+            for(int i=0;i<d.length;i++)
+            {
+                if(space<6)
+                {
+                    String str=String.valueOf(d[i]);
+                    if(str.length()>2)
+                    {
+                        System.out.print(d[i]+"  ");
+                    }
+                    else
+                    {
+                        System.out.print(d[i]+"   ");
+                    }
+                }
+                else
+                {
+                    System.out.println(d[i]);
+                    space=space-6;
+                }
+                space++;
+            }
+
         }
     }
 }
